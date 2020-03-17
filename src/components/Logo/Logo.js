cf7d0bf991d2d9ca27f8ae9335bc62fc2f5a5aad
UI Galaxy <uigalaxy7@gmail.com>
2020-03-17 17:25:16 +0600

    fix: logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,50 +1,7 @@
 import React from "react";
 
-const Logo = ({ footer, height, className="" }) => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     wordpressAcfHome {
-  //       acf {
-  //         common {
-  //           header_logo {
-  //             localFile {
-  //               childImageSharp {
-  //                 fixed(width: 165) {
-  //                   ...GatsbyImageSharpFixed
-  //                 }
-  //               }
-  //             }
-  //           }
-  //           footer_logo {
-  //             localFile {
-  //               childImageSharp {
-  //                 fixed(width: 165) {
-  //                   ...GatsbyImageSharpFixed
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-
-  // const headerImg = data.wordpressAcfHome.acf.common.header_logo.localFile.childImageSharp.fixed;
-  // const footerImg = data.wordpressAcfHome.acf.common.footer_logo.localFile.childImageSharp.fixed;
-
-  return (
-    <div className={`${className}`}>
-      {/* <Img
-        fixed={footer ? footerImg : headerImg}
-        css={`
-          height: ${height || "auto"};
-        `}
-        alt="Logo"
-      /> */}
-      Logo
-    </div>
-  );
+const Logo = ({ footer, height, className = "" }) => {
+  return <div className={`${className}`}>Logo</div>;
 };
 
 export default Logo;
